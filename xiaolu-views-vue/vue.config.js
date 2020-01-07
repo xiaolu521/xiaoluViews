971da0b5b0de2ca24b4fadf0b3b0779ffaf3c0b6
majingcheng <ma123456>
2020-01-07 09:05:30 +0800

    1

--- xiaolu-views-vue/vue.config.js
+++ xiaolu-views-vue/vue.config.js
@@ -12,6 +12,7 @@ module.exports = {
     https://github.com/vuejs/vue-cli/issues/2463
    */
   // 如果你不需要生产环境的 source map，可以将其设置为 false 以加速生产环境构建。
+
   publicPath: process.env.NODE_ENV === "production" ? "././" : "/",
   assetsDir:'static',
   productionSourceMap: false,
@@ -37,6 +38,8 @@ module.exports = {
     }
   },
   devServer: {
+    disableHostCheck: true,
+    host:'http://www.xiaolus.com:8080/',
     port: 8080,
     proxy: {
       '/api/': {
